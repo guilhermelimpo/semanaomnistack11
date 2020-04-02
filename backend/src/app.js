b34f97f971851e130d8079041bf774cb03f2226a
Guilherme Limpo <guilhermelimpo@gmail.com>
2020-04-02 19:41:48 -0300

    Valição no backend + tests TDD

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -10,5 +11,6 @@ app.use(cors());
 // Aqui é para dizer que o body vai vir em JSON
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
